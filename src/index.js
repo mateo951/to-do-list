0a64163b344afab2a74a9d46e3a9e588fab08000
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-08-21 11:40:27 -0500

    Add remove & clear all completed functionality & Linters

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import './style.css';
 import Interactions from './modules/interactions.js';
 import Tasks from './modules/tasks.js';
 
-document.querySelector('#submitBttn').addEventListener('click', () => {
+document.querySelector('#formTask').addEventListener('click', () => {
   Interactions.addTask();
+  const input = document.querySelector("#formTask input[type='text']");
+  input.value = '';
 });
 
 document.querySelector('#formTask').addEventListener('submit', () => {
   Interactions.addTask();
+  const input = document.querySelector("#formTask input[type='text']");
+  input.value = '';
 });
 
-function CheckLocalInput() {
+const checkLocalInput = () => {
   const data = JSON.parse(localStorage.getItem('data'));
   if (data !== null) {
     Tasks.initializeTasks(data);
@@ -22,6 +26,64 @@ function CheckLocalInput() {
       }
     }
   }
-}
+};
 
-CheckLocalInput();
+const edit = function (taskH3) {
+  const editInput = document.getElementById(`e${taskH3.id.substring(1)}`);
+  const h3Text = document.getElementById(`d${taskH3.id.substring(1)}`);
+  const deleteIcon = document.getElementById(`i${taskH3.id.substring(1)}`);
+  const optionIcon = document.getElementById(`o${taskH3.id.substring(1)}`);
+  const editLi = document.getElementById(`l${taskH3.id.substring(1)}`);
+
+  if (taskH3 && editLi && editInput && h3Text && deleteIcon && optionIcon) {
+    Interactions.editMode = !Interactions.editMode;
+    if (editInput.value !== Tasks.tasksData[taskH3.id.substring(1)].description) {
+      if (!Interactions.editMode && editInput.value === '' && taskH3.tagName === 'INPUT') {
+        Interactions.editMode = !Interactions.editMode; /* eslint-disable-next-line */
+        removeTask(taskH3);
+      } else if (Interactions.hasValue(editInput.value)) {
+        Interactions.updateDesc(editInput.value, taskH3.id.substring(1), h3Text);
+      }
+    }
+
+    h3Text.classList.toggle('edit');
+    editLi.classList.toggle('editLi');
+    optionIcon.classList.toggle('edit');
+    editInput.classList.toggle('edit');
+    deleteIcon.classList.toggle('edit');
+    if (Interactions.editMode) {
+      editInput.value = h3Text.innerText;
+      editInput.focus();
+    }
+  }
+};
+
+window.toggleEdit = function (taskH3) {
+  setTimeout(() => { edit(taskH3); }, 100);
+};
+
+window.removeTask = function (task) {
+  Interactions.editMode = !Interactions.editMode;
+  const taskSelected = document.getElementById(task.id);
+  Interactions.removeTask(taskSelected.id.substring(1), false);
+};
+
+const clearCompleted = () => {
+  const bttn = document.getElementById('clearCompleted');
+  bttn.addEventListener('click', () => {
+    const checkboxs = document.getElementsByClassName('checkbox');
+    if (checkboxs) {
+      Tasks.tasksData = Tasks.tasksData.filter((task) => !task.completed);
+      const ul = document.getElementById('spawnTasks');
+      ul.innerHTML = '';
+      for (let i = 0; i < Tasks.tasksData.length; i += 1) {
+        Tasks.tasksData[i].index = i;
+        Interactions.updateDisplay(Tasks.tasksData[i]);
+      }
+      Interactions.CheckInput();
+    }
+  });
+};
+
+clearCompleted();
+checkLocalInput();
--- src/modules/interactions.js
+++ src/modules/interactions.js
@@ -2,6 +2,8 @@ import Task from './task.js';
 import Tasks from './tasks.js';
 
 export default class Interactions {
+  static editMode = false;
+
   static addTask() {
     const inputData = document.getElementById('inputTask');
     if (this.hasValue(inputData.value)) {
@@ -12,18 +14,50 @@ export default class Interactions {
     }
   }
 
+  static removeTask(task) {
+    const liTask = document.getElementById(`l${task}`);
+    this.rearrangeIds(task);
+    Tasks.tasksData.splice(task, 1); // eslint-disable-next-line no-use-before-define
+    this.CheckInput();
+    liTask.remove();
+  }
+
+  static rearrangeIds(task) {
+    if (task !== Tasks.tasksData.length) {
+      for (let i = parseInt(task, 10); i < Tasks.tasksData.length - 1; i += 1) {
+        const checkbox = document.getElementById(`${i + 1}`);
+        const newLi = document.getElementById(`l${i + 1}`);
+        const editInput = document.getElementById(`e${i + 1}`);
+        const h3Text = document.getElementById(`d${i + 1}`);
+        const deleteIcon = document.getElementById(`i${i + 1}`);
+        const optionIcon = document.getElementById(`o${i + 1}`);
+        Tasks.tasksData[i + 1].index = i;
+        checkbox.id = i;
+        newLi.id = `l${i}`;
+        editInput.id = `e${i}`;
+        h3Text.id = `d${i}`;
+        deleteIcon.id = `i${i}`;
+        optionIcon.id = `o${i}`;
+      }
+    }
+  }
+
   static updateDisplay(task) {
-    const ul = document.querySelector('ul');
-    ul.insertAdjacentHTML('beforeend', `<li><input type="checkbox" id="${task.index}"><h3 id="d${task.index}">${task.description}</h3><img src="https://img.icons8.com/ios-glyphs/30/000000/menu-2.png"/>`);
+    const ul = document.querySelector('#spawnTasks');
+    ul.insertAdjacentHTML('beforeend', `<li id="l${task.index}">
+    <input class="checkbox" type="checkbox" id="${task.index}">
+    <h3 id="d${task.index}" onclick="toggleEdit(this)">${task.description}</h3>
+    <input id="e${task.index}" class="edit editInput" onblur="toggleEdit(this)"></input>
+    <img class="edit deleteIcon" onclick="removeTask(this)" id="i${task.index}" src="https://img.icons8.com/ios/50/000000/delete--v1.png"/>
+    <img id="o${task.index}" src="https://img.icons8.com/ios-glyphs/30/000000/menu-2.png"/></li>`);
     this.addCheckboxEvent(task);
   }
 
-  static hasValue(input) {
-    if (input === '') {
-      return false;
-    }
-    return true;
-  }
+  static updateDesc = (input, index, h3) => {
+    Tasks.tasksData[index].description = input;
+    h3.innerText = input;
+    this.CheckInput();
+  };
 
   static addCheckboxEvent(task) {
     const checkbox = document.getElementById(task.index);
@@ -32,6 +66,13 @@ export default class Interactions {
       const desc = document.getElementById(`d${task.index}`);
       desc.classList.toggle('line-through');
     });
+  }
+
+  static hasValue(input) {
+    if (input === '') {
+      return false;
+    }
+    return true;
   }
 
   static CheckInput() {
